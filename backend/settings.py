01eabda17e825d2dc6ccc21de361b8082728a2ff
hypotyposis <32574270+hypotyposis@users.noreply.github.com>
2021-08-30 00:05:43 +0800

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')$d8fvi35&ix*wdtzjwlxv$1o2+^mas+na*$q2o#=12lb^q7cg'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
